084a2c33ff53dde5fc2d8d3a03c4e2f3f620d5fc
deepcloudlabs <deepcloudlabs@gmail.com>
2024-03-18 15:55:37 +0300

    Create Exercise01.java

---
+++ module01-new.features/src/com/example/java11/Exercise01.java
@@ -0,0 +1,18 @@
+package com.example.java11;
+
+public class Exercise01 {
+	public static void main(String[] args) {
+	   Test test1 = (x,y) -> x+y;		
+	   Test test2 = (double x,double y) -> x+y;		
+	   Test test3 = (var x,var y) -> x+y;		
+	   Test test4 = (final double x,@A double y) -> x+y;		
+	   Test test5 = (final var x,@A var y) -> x+y;		
+	   
+	}
+}
+
+@interface A {}
+@FunctionalInterface 
+interface Test { 
+ public abstract double math (double x, double y);
+}
