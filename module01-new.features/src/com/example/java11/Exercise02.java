9495b31ba04d45ec928b71a546e69664308cca67
deepcloudlabs <deepcloudlabs@gmail.com>
2024-03-18 15:55:49 +0300

    Create Exercise02.java

---
+++ module01-new.features/src/com/example/java11/Exercise02.java
@@ -0,0 +1,14 @@
+package com.example.java11;
+
+public class Exercise02 {
+
+	public static void main(String[] args) {
+		String multiLine = """
+Quantum computing is a type of computation
+whose operations can harness the phenomena of quantum mechanics,
+such as superposition, interference, and entanglement.
+				""";
+		System.out.println(multiLine);
+	}
+
+}
