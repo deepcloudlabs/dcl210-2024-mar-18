67b37cae43ca210150da78d570e55fc75e4aa523
Binnur KURT <deepcloudlabs@gmail.com>
2024-03-20 13:40:02 +0300

    Update Exercise01.java

--- study-concurrency-api/src/com/example/Exercise01.java
+++ study-concurrency-api/src/com/example/Exercise01.java
@@ -1,9 +1,12 @@
 package com.example;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Exercise01 {
+	public static final ExecutorService threadPool = Executors.newFixedThreadPool(20);
 
 	public static void main(String[] args) {
 		var service = new BusinessService();
@@ -16,6 +19,7 @@ public class Exercise01 {
 		});
 		System.out.println("Application is done.");
 		try {TimeUnit.SECONDS.sleep(15);} catch (InterruptedException e) {}
+		threadPool.shutdown();
 	}
 
 }
@@ -28,8 +32,9 @@ class BusinessService {
 	
 	public CompletableFuture<Integer> gun() { // asynchronous
 		return CompletableFuture.supplyAsync(() -> {
+			System.out.println("%s runs CompletableFuture.supplyAsync()".formatted(Thread.currentThread().getName()));
 			try {TimeUnit.SECONDS.sleep(5);} catch (InterruptedException e) {}
 			return 42;
-		});
+		},Exercise01.threadPool);
 	}
 }
